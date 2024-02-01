e05ea66da6b026debe2aaf4a39007c8e8a3e7a5d
Thomas Pappas <thomaskpappas@gmail.com>
2024-02-01 15:40:08 -0500

    npm packages setup

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react'
 
 const App = () => {
     return (
-        <BrowserRouter>
-            <Routes>
-            </Routes>
-        </BrowserRouter>
+        <ChakraProvider>
+            <BrowserRouter>
+                <Routes>
+                </Routes>
+            </BrowserRouter>
+        </ChakraProvider>
     );
   };
   
